382e0316cc2e40799c30e79cd43cceed0d14b1c1
Dragoljub Kojadinovic <kojadinovicdragoljub@gmail.com>
2020-04-09 13:12:04 +0200

    Remove onViewCreated in FirstFragment
    Remove unused name param in fragment_in_fragment and replace_fragment xmls

--- Vezbe7/app/src/main/java/rs/raf/vezbe7/view/fragments/FirstFragment.kt
+++ Vezbe7/app/src/main/java/rs/raf/vezbe7/view/fragments/FirstFragment.kt
@@ -1,14 +1,6 @@
 package rs.raf.vezbe7.view.fragments
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import rs.raf.vezbe7.R
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-}
+class FirstFragment : Fragment(R.layout.fragment_first)
