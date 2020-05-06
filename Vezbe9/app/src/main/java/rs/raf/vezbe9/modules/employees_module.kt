8cbf264894f72356cd56c8873badfa2e47b44e0d
Dragoljub Kojadinovic <kojadinovicdragoljub@gmail.com>
2020-05-07 00:32:17 +0200

    Init employees koin module

--- Vezbe9/app/src/main/java/rs/raf/vezbe9/modules/employees_module.kt
+++ Vezbe9/app/src/main/java/rs/raf/vezbe9/modules/employees_module.kt
@@ -1,7 +1,18 @@
 package rs.raf.vezbe9.modules
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import rs.raf.vezbe9.data.datasources.EmployeeDataSource
+import rs.raf.vezbe9.data.repository.EmployeeRepository
+import rs.raf.vezbe9.data.repository.EmployeeRepositoryImpl
+import rs.raf.vezbe9.presentation.viewmodel.MainViewModel
 
 val employeesModule = module {
+
+    viewModel { MainViewModel(get()) }
+
+    single<EmployeeRepository> { EmployeeRepositoryImpl(get()) }
+
+    single<EmployeeDataSource> { create(get()) }
 
 }
