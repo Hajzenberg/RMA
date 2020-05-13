dd207f17b1ce7b47658b3fa766b60b654cbf3e1c
Dragoljub Kojadinovic <kojadinovicdragoljub@gmail.com>
2020-05-14 00:19:26 +0200

    Skeleton complete

--- Vezbe10/app/src/main/java/rs/raf/vezbe10/presentation/views/activities/MainActivity.kt
+++ Vezbe10/app/src/main/java/rs/raf/vezbe10/presentation/views/activities/MainActivity.kt
@@ -2,12 +2,26 @@ package rs.raf.vezbe10.presentation.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import rs.raf.vezbe10.R
+import rs.raf.vezbe10.data.local.models.Student
+import rs.raf.vezbe10.presentation.contract.MainContract
+import rs.raf.vezbe10.presentation.viewmodels.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val mainViewModel: MainContract.ViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        initObservers()
+    }
+
+    private fun initObservers() {
+        
     }
 }
