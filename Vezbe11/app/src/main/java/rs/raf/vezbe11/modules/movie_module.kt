b2dc07f81071f2e50c97db310927f7c138ae23df
Dragoljub Kojadinovic <kojadinovicdragoljub@gmail.com>
2020-05-21 01:24:22 +0200

    Fixes

--- Vezbe11/app/src/main/java/rs/raf/vezbe11/modules/movie_module.kt
+++ Vezbe11/app/src/main/java/rs/raf/vezbe11/modules/movie_module.kt
@@ -3,6 +3,7 @@ package rs.raf.vezbe11.modules
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import rs.raf.vezbe11.data.datasources.local.MovieDataBase
+import rs.raf.vezbe11.data.datasources.remote.MovieService
 import rs.raf.vezbe11.data.repositories.MovieRepository
 import rs.raf.vezbe11.data.repositories.MovieRepositoryImpl
 import rs.raf.vezbe11.presentation.viewmodel.MainViewModel
@@ -14,6 +15,8 @@ val movieModule = module {
     single<MovieRepository> { MovieRepositoryImpl(localDataSource = get(), remoteDataSource = get()) }
 
     single { get<MovieDataBase>().getMovieDao() }
+
+    single<MovieService> { create(retrofit = get()) }
 
 }
 
